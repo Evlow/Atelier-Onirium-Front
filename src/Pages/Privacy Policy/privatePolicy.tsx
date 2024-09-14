13c1cb40a056a6854ed0232e29cc153797c27fdf
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-09-14 23:53:00 +0200

    add private policy

---
+++ src/Pages/Privacy Policy/privatePolicy.tsx
@@ -0,0 +1,188 @@
+import Footer from "../../Components/Footer/footer";
+import NavBar from "../../Components/NavBar/navbar";
+
+export default function PrivatePolicy() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <section>
+        <article>
+          <p>
+            Cette politique s'applique au site : www.latelierdonirium.fr Date de
+            dernière mise à jour 13 Août 2024 Le respect de votre vie privée est
+            de la plus haute importance pour Marion DUIZABO, qui est responsable
+            de ce site. La présente politique de confidentialité a pour but de
+            vous exposer : La manière dont sont collectés et traités vos
+            renseignements personnels. Doivent être considérés comme
+            renseignements personnels tous les renseignements susceptibles de
+            vous identifier. Il s'agit notamment de votre prénom et nom, de
+            votre âge, de votre adresse postale, de votre adresse courriel de
+            votre localisation en encore de votre adresse IP. « Renseignements
+            personnels » est également utilisé comme synonyme de données
+            personnelles au sens du Règlement européen 2016/679 ; Quels sont vos
+            droits concernant ces renseignements ; Qui est responsable du
+            traitement des renseignements personnels collectés et traités ; A
+            qui ces renseignements sont transmis ; Eventuellement, la politique
+            du site en matière de fichiers témoins (« cookies »). Cette
+            politique de confidentialité complète les Conditions Générales de
+            Vente que vous pouvez consulter ICI 1 COLLECTE DE RENSEIGNEMENTS
+            PERSONNELS Nous collectons les renseignements personnels suivants :
+            Nom Prénom Adresse postale Code postal Adresse électronique Numéro
+            de téléphone Numéro de carde de crédit Genre ou sexe Les
+            renseignements personnels que nous collectons sont recueillis par
+            l'entremise des modes de collecte décrits ci-bas, dans les sections
+            « Formulaires et modes de collecte » et suivantes. 2 FORMULAIRES ET
+            MODE DE COLLECTE Vos renseignements personnels sont collectés par
+            l'entremise des méthodes suivantes : Formulaire d'inscription au
+            site Web Formulaire de commande Nous utilisons les renseignements
+            ainsi collectés pour les finalités suivantes : Suivis de commande
+            Informations / Offres promotionnelles. Statistiques, Contact Gestion
+            du site web 3 INTERACTIVITE Vos renseignements personnels sont
+            également collectés par le biais de l'interactivité pouvant
+            s'établir entre vous et notre site Web. Ces renseignements sont
+            collectés par les moyens suivants : Correspondance Nous utilisons
+            les renseignements ainsi collectés pour les finalités suivantes :
+            Statistiques, Contact, Gestion du site web 4 PARTAGE DES
+            RENSEIGNEMENTS PERSONNELS Les renseignements personnels collectés
+            par le site ne sont transmis à aucun tiers et ne sont traités que
+            par nous ; 5 DUREE DE CONSERVATION DES RENSEIGNEMENTS PERSONNELS Le
+            responsable du traitement des renseignements personnels conservera
+            dans ses systèmes informatiques du site et dans des conditions
+            raisonnables de sécurité l'ensemble des renseignements personnels
+            collectés pour une durée de 12 mois. 6 HEBERGEMENT DES
+            RENSEIGNEMENTS PERSONNELS Notre site est hébergé par 1 & 1 IONOS,
+            dont le siège est situé à l'adresse ci-après : 7 Place de la Gare
+            5720 SARREGUEMINES 7 RESPONSABLE DU TRAITEMENT DES RENSEIGNEMENTS
+            PERSONNELS a) La responsable du traitement des renseignements
+            personnels est : Marion DUIZABO. Elle peut être contactée de la
+            manière suivante : contact@latelierdonirium.fr La responsable du
+            traitement des renseignements personnels est chargée de déterminer
+            les finalités et les moyens mis au service du traitement des
+            renseignements personnels. b) Obligations du responsable du
+            traitement des renseignements personnels La responsable du
+            traitement des renseignements personnels s'engage à protéger les
+            renseignements personnels collectés, à ne pas les transmettre à des
+            tiers sans que vous n'en ayez été informé (e) et à respecter les
+            finalités pour lesquelles ces renseignements ont été collectés. Dans
+            le cas où l'intégrité, la confidentialité ou la sécurité de vos
+            renseignements personnels est compromise, la responsable du
+            traitement s'engage à vous en informer par tout moyen. DROIT
+            D'OPPOSITION ET DE RETRAIT Vous avez le droit de vous opposer au
+            traitement de vos renseignements personnels par le site (droit
+            d'opposition). Vous avez également le droit de demande à ce que vos
+            renseignements personnels ne figurent plus, par exemple, dans une
+            liste de diffusion (droit de retrait). Afin de formuler une
+            opposition au traitement de vos renseignements personnels ou
+            demander le retrait de vos renseignements personnels, vous devez
+            suivre la procédure suivante : L'utilisateur doit faire une demande
+            par mail via contact@latelierdonirium.fr DROIT D'ACCES, DE
+            RECTIFICATION ET DE SUPPRESSION Vous pouvez prendre connaissance,
+            mettre à jour, modifier ou demander la suppression des
+            renseignements vous concernant en respectant la procédure ci-après
+            énoncée : Mail via contact@latelierdonirium.fr Si vous en possédez,
+            vous avez le droit de demander la suppression de votre espace
+            personnel en suivant la procédure suivante : L'utilisateur doit
+            envoyer une demande par mail à contact@latelierdonirium.fr PRINCIPES
+            GENERAUX EN MATIERE DE COLLECTE ET DE TRAITEMENT DES RENSEIGNEMENTS
+            PERSONNELS EN VERTU DU REGLEMENT EUROPEEN 2016/679 ; Conformément
+            aux dispositions de l'article 5 du Règlement européen 2016/679, la
+            collecte et le traitement de vos renseignements personnels
+            respectent les principes suivants : Licéité, loyauté et
+            transparence : vos renseignements personnels ne peuvent être
+            collectés et traités qu'avec votre consentement. A chaque fois que
+            des renseignements persones seront collectés, il sera indiqué que
+            vos renseignements personnels sont collectés et pour quelles raisons
+            vos renseignements personnels sont collectés, Finalités limitées :
+            la collecte et le traitement des renseignements personnels sont
+            exécutés pour répondre à un ou plusieurs objectifs déterminés dans
+            la présente politique de confidentialité. Minimalisation de la
+            collecte et du traitement des renseignements personnels : seuls les
+            renseignements personnes nécessaires à la bonne exécution des
+            objectifs poursuivis par le site sont collectés ; Conservation des
+            renseignements personnels réduites dans les temps : les
+            renseignements personnels sont conservés pour une durée limitée dont
+            vous êtes informés ; Intégrité et confidentialité des renseignements
+            personnels collectés et traités : la responsable du traitement des
+            renseignements personnels s'engage à garantir l'intégrité et la
+            confidentialité des renseignements personnels collectés. Afin d'être
+            licites, et conformémentaux exigences de l'article 6 du Règlement
+            européen 2016/679, la collecte et le traitement des renseignements
+            personnels ne pourront intervenir que s'ils respectent au moins
+            l'une des conditions ci-après énumérés : Vous avez expressément
+            consenti au traitement ; Le traitement est nécessaire à l'exécution
+            d'un contrat. Le traitement répond à une obligation légale. Le
+            traitement s'explique par la nécessité de sauvegarde de vos intérêts
+            vitaux ou de ceux d'une autre personne physique. Le traitement peut
+            s'expliquer par une nécessité liée à l'exécution d'une mission
+            d'intérêt public ou qui relève de l'exercice de l'autorité publique.
+            Le traitement et la collecte des renseignements personnels sont
+            nécessaires aux intérêts légitimes et privés poursuivis par le
+            responsable du traitement des renseignements personnels ou par un
+            tiers. DROITS ADDITIONNELS EN VERTU DU REGLEMENT EUROPEEN 2016/679
+            Conformément à la réglementation européenne concernant le traitement
+            des renseignements personnels vous possédez également les droits
+            ci-après énumérés. Afin que la responsable du traitement des
+            renseignements fasse droit à votre demande, vous êtes tenus de lui
+            communiquer vos prénom et nom ainsi que votre adresse courriel, et
+            si cela est pertinent, votre numéro de compte ou d'espace personnel
+            ou d'abonné. Le responsable du traitement des renseignements
+            personnels est tenu de vous répondre dans un délai de trente jours
+            maximum. a) Droit à la portabilité des renseignements personnels
+            Vous avez le droit de demander la portabilité de vos renseignements
+            personnels, détenus par le site, vers en autre site, en vous
+            conformant à la procédure ci-après : L'utilisateur doit effectuer
+            une demande par mail via contact@latelierdonirium.fr b) Droit de ne
+            pas faire l'objet d'une décision fondée exclusivement sur un procédé
+            automatisé. Conformément aux dispositions du règlement européen
+            2019/679, vous avez le droit de ne pas faire l'objet d'une décision
+            fondée exclusivement sur un procédé automatisé si la décision
+            produit des effets juridiques vous concernant ou affecte de manière
+            significative de façon similaire. Droit de saisir l'autorité
+            compétente Dans le cas où le responsable du traitement des
+            renseignements personnels décide de ne pas répondre à votre demande
+            et que vous souhaitez contester cette décisio ou, si vous pensez
+            qu'il est porté atteinte à l'un de vos droits énumérés ci-dessus,
+            vous êtes en deroit de saisir toute cour compétente. 12 – SECURITE
+            Les informations personnelles que nous collectons sont conservées
+            dans un environnement sécurisé. Pour assurer la sécurité de vos
+            informations personnelles, nous avons recours aux mesures
+            suivantes : Protocole SSL (Secure Sockets Layer) Sauvegarde
+            automatique Identifiant / mot de passe Nous nous engageons à
+            maintenir un haut degré de confidentialité en intégrant les
+            dernières innovations technologiques permettant d'assurer la
+            confidentialité de vos transactions. Toutefois, comme aucun
+            mécanisme n'offre une sécurité maximale, une part de risque est
+            toujours présente lorsqu'on utilise internet pour transmettre des
+            informations personnelles. 13 RENSEIGNEMENTS PERSONNELS DES
+            PERSONNES MINEURES EN VERTU DU REGLEMENT 2016/679 Conformément aux
+            dispositions de l'article 8 du règlement 2016/679, seuls les mineurs
+            âgés de 15 ans ou plus peuvent consentir au traitement de leurs
+            renseignements personnels. Si vous êtes un mineur de moins de 15
+            ans, l'accord d'un représentant légal sera requis afin que des
+            renseignements personnels puissent être collectés et traités. Nous
+            nous réservons le droit de vérifier par tout moyen que vous êtes agé
+            de plus de 15 ans, ou que vous avez obtenu l'accord d'un
+            représentant légal avant de naviguer sur le site. 14 CONDITIONS DE
+            MODIFICATION DE LA POLITIQUE DE CONFIDENTIALITE La présente
+            politique de confidentialité peut être consultée à tout moment à
+            l'adresse ci-après indiquée :
+            https://latelierdonirium.fr/politique-de-confidentialite Nous nous
+            réservons le droit de la modifier afin de garantir sa conformité
+            avec le droit en vigueur. Par conséquent, vous êtes invité (e) à
+            venir consulter régulièrement cette politique de confidentialité
+            afin de vous tenir informé (e) des derniers changements qui lui
+            seront apportés. LEGISLATION Nous nous engageons à respecter les
+            dispositions législatives énonces ci-dessus et le Réglement Général
+            sur la protection des données, Règlement (UE) 2016/679 du Parlement
+            européen et du Conseil du 27 Avril 2016 relatif à la protection des
+            personnes physiques à l'égard du traitement des données à caractère
+            personnel et à la libre circulation de ces données et abrogeant la
+            directive 95/46ECE (règlement général sur la protection des données)
+            2016/279
+          </p>
+        </article>
+      </section>
+      <Footer></Footer>
+    </div>
+  );
+}
