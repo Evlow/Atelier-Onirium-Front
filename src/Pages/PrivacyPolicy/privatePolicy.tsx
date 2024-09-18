f561deb968fc7eae81521d1fea56228ae681a491
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-09-18 15:22:41 +0200

    update privatePolicy

--- src/Pages/PrivacyPolicy/privatePolicy.tsx
+++ src/Pages/PrivacyPolicy/privatePolicy.tsx
@@ -43,7 +43,7 @@ export default function PrivatePolicy() {
         <h3 className="h3-private">COLLECTE DES RENSEIGNEMENTS PERSONNELS</h3>
         <p>
           Les renseignements personnels suivants seront collectés :
-          <ul>
+          <ul className="ul-private">
             <li>Nom</li>
             <li>Prénom</li>
             <li>Adresse postale</li>
@@ -55,29 +55,185 @@ export default function PrivatePolicy() {
           </ul>
         </p>
         <p>
-          Les renseignements personnels recueillis sont obtenus par
-          les méthodes décrites dans les sections « Formulaires et modes de
-          collecte » et les suivantes.
+          Les renseignements personnels recueillis sont obtenus par les méthodes
+          décrites dans les sections « Formulaires et modes de collecte » et les
+          suivantes.
         </p>
         <h3 className="h3-private">FORMULAIRES ET MODE DE COLLECTE</h3>
-<p>Vos renseignements personnels sont collectés par les méthodes suivantes : </p>
-<ul><li>Formulaire d'inscription au site internet</li>
-<li>Formulaire de commande</li>
-<li>Formulaire de contact</li>
-<li>Suivis de commande</li>
-<li>Gestion du site internet</li>
-<li>Informations et offres promotionnelles</li>
-<li>Statistiques</li>
-</ul>
-<h3 className="h3-private">PARTAGE DES RENSEIGNEMENTS PERSONNELS</h3>
- <p>Les renseignements personnels collectés par le site ne sont transmis à aucun tiers et ne sont traités que par l'atelier d'Onirium</p>
- <h3 className="h3-private">HÉBERGEMENT DU SITE INTERNET</h3>
-    <p>Le site  www.latelierdonirium.fr est hébergé par 1 & 1 IONOS, dont le siège est situé à l'adresse ci-dessous :
-7 Place de la Gare 
-<br />
-5720 SARREGUEMINES
-</p>
-      
+        <p>
+          Vos renseignements personnels sont collectés par les méthodes
+          suivantes :{" "}
+        </p>
+        <ul className="ul-private">
+          <li>Formulaire d'inscription au site internet</li>
+          <li>Formulaire de commande</li>
+          <li>Formulaire de contact</li>
+          <li>Suivis de commande</li>
+          <li>Gestion du site internet</li>
+          <li>Informations et offres promotionnelles</li>
+          <li>Statistiques</li>
+        </ul>
+        <h3 className="h3-private">PARTAGE DES RENSEIGNEMENTS PERSONNELS</h3>
+        <p>
+          Les renseignements personnels collectés par le site ne sont transmis à
+          aucun tiers et ne sont traités que par l'atelier d'Onirium
+        </p>
+        <h3 className="h3-private">HÉBERGEMENT DU SITE INTERNET</h3>
+        <p>
+          Le site www.latelierdonirium.fr est hébergé par 1 & 1 IONOS, dont le
+          siège est situé à l'adresse ci-dessous : 7 Place de la Gare
+          <br />
+          5720 SARREGUEMINES
+        </p>
+        <h3 className="h3-private">
+          RESPONSABLE DU TRAITEMENT DES RENSEIGNEMENTS PERSONNELS
+        </h3>
+        <p>
+          La responsable du traitement des renseignements personnels est :
+          Marion DUIZABO. <br /> Elle peut être contactée de la manière suivante
+          : contact@latelierdonirium.fr
+        </p>
+        <p>
+          La responsable du traitement des renseignements personnels s'engage à
+          protéger les renseignements personnels collectés, à ne pas les
+          transmettre à des tiers sans que vous n'en ayez été informé.e et à
+          respecter les finalités pour lesquelles ces renseignements ont été
+          collectés.
+        </p>
+        <p>
+          Dans le cas où l'intégrité, la confidentialité ou la sécurité de vos
+          renseignements personnels est compromise, la responsable du traitement
+          s'engage à vous en informer par tout moyen.
+        </p>
+        <h3 className="h3-private">DROIT D'OPPOSITION ET DE RETRAIT</h3>
+        <p>
+          Vous avez le droit de vous opposer au traitement de vos renseignements
+          personnels par le site (droit d'opposition). <br />
+          Vous avez également le droit de demande à ce que vos renseignements
+          personnels ne figurent plus, par exemple, dans une liste de diffusion
+          (droit de retrait).
+        </p>
+        <p>
+          L'utilisateur doit faire une demande par mail via
+          contact@latelierdonirium.fr
+        </p>
+        <h3 className="h3-private">
+          DROIT D'ACCES, DE RÉCTIFICATION ET DE SUPPRESSION
+        </h3>
+        <p>
+          Vous pouvez consulter, mettre à jour, modifier ou demander la
+          suppression de vos renseignements personnels en envoyant un email à
+          contact@latelierdonirium.fr <br />
+          Si vous souhaitez supprimer votre espace personnel, vous pouvez le
+          faire en envoyant une demande par email à contact@latelierdonirium.fr
+        </p>
+        <h3 className="h3-private">
+          CONDITIONS DE MODIFICATION DE LA POLITIQUE DE CONFIDENTIALITÉ
+        </h3>
+        <p>
+          La présente politique de confidentialité peut être consultée à tout
+          moment à l'adresse ci-après :
+          https://latelierdonirium.fr/politique-de-confidentialite
+        </p>
+        <p>
+          {" "}
+          L'atelier d'onirium se réserve le droit de la modifier afin de
+          garantir sa conformité avec le droit en vigueur. Par conséquent, vous
+          êtes invité.e à venir consulter régulièrement cette politique de
+          confidentialité afin de vous tenir informé.e des derniers changements
+          qui lui seront apportés.
+        </p>
+        <h3 className="h3-private">
+          PRINCIPES FONDAMENTAUX DU RÈGLEMENT EUROPÉEN 2016/679 SUR LA
+          PROTECTION DES DONNÉES
+        </h3>
+        <p>
+          Conformément aux dispositions de l'article 5 du Règlement Européen
+          (UE) 2016/679, la collecte et le traitement de vos renseignements
+          personnels respectent les principes suivants :
+        </p>
+        <h3 className="h3-private">LÉGISLATION</h3>
+        <p>
+          L'atelier d'onirium s'engage à respecter les dispositions législatives
+          mentionnées ci-dessus ainsi que le Règlement Général sur la Protection
+          des Données (RGPD), Règlement (UE) 2016/679 du Parlement européen et
+          du Conseil du 27 avril 2016, relatif à la protection des personnes
+          physiques à l’égard du traitement des données à caractère personnel et
+          à la libre circulation de ces données, et abrogeant la directive
+          95/46/CE.
+        </p>
+        <ul className="ul-private">
+          <li>
+            <strong>Licéité, loyauté et transparence :</strong> Vos renseignements personnels
+            sont collectés et traités uniquement avec votre consentement. Chaque
+            collecte est clairement indiquée, en expliquant pourquoi vos données
+            sont recueillies.
+          </li>
+          <li>
+            <strong>Finalités limitées :</strong>Les renseignements personnels
+            sont collectés et traités pour des objectifs spécifiques, tels que
+            décrits dans cette politique de confidentialité.
+          </li>
+          <li>
+            <strong> Minimisation :</strong>Seules les données nécessaires pour
+            atteindre les objectifs sont collectées.
+          </li>
+          <li>
+            <strong>Conservation limitée :</strong>Les données sont conservées
+            pour une durée limitée, dont vous êtes informé.
+          </li>
+          <li>
+            <strong>Intégrité et confidentialité : </strong>Nous nous engageons
+            à garantir l'intégrité et la confidentialité de vos renseignements
+            personnels.
+          </li>
+        </ul>
+        <p>
+          Conformément à l'article 6 du Règlement Européen (UE) 2016/679, la
+          collecte et le traitement des renseignements personnels sont légaux
+          uniquement si l'une des conditions suivantes est remplie :
+        </p>
+        <ul className="ul-private">
+          <li>Vous avez donné votre consentement.</li>
+          <li>Le traitement est nécessaire pour l'exécution d'un contrat.</li>
+          <li>Le traitement est requis par la loi.</li>
+          <li>
+            Le traitement est nécessaire pour protéger vos intérêts vitaux ou
+            ceux d'une autre personne.
+          </li>
+          <li>
+            Le traitement est nécessaire pour l'exécution d'une mission
+            d'intérêt public ou relevant de l'exercice de l'autorité publique.
+          </li>
+          <li>
+            Le traitement est nécessaire aux intérêts légitimes poursuivis par
+            nous ou un tiers.
+          </li>
+        </ul>
+        <p>
+          Droits supplémentaires en vertu du Règlement Européen (UE) 2016/679
+        </p>
+        <ul className="ul-private">
+          <li>
+            <strong>Droit à la portabilité :</strong> Vous pouvez demander la
+            portabilité de vos données vers un autre site.
+            <br /> Faites la demande par
+            mail à l'adresse suivante : contact@latelierdonirium.fr
+          </li>
+          <li>
+            <strong>
+              Droit de ne pas faire l'objet d'une décision automatisée :
+            </strong>
+            Vous avez le droit de ne pas être soumis à une décision basée
+            exclusivement sur un traitement automatisé qui a des effets
+            juridiques ou significatifs sur vous.
+          </li>
+          <li>
+            <strong>Droit de saisir l'autorité compétente :</strong> Si vous
+            êtes insatisfait de la réponse à votre demande ou si vous pensez que
+            vos droits ont été violés, vous pouvez saisir l'autorité compétente.
+          </li>
+        </ul>
       </section>
       <Footer></Footer>
     </div>
