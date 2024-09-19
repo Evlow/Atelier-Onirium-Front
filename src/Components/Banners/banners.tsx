a1d19f5ea796f26c1b0318dcc993b86ebbd62e8e
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-09-19 15:04:13 +0200

    update files

--- src/Components/Banners/banners.tsx
+++ src/Components/Banners/banners.tsx
@@ -2,11 +2,14 @@ import "./banners.css";
 
 interface Props {
   imgBanner: string;
+  textBanner? : string | null;
+  positionText? : "center"
 }
 export default function Banners(Props: Props) {
   return (
     <div className="banner">
-      <img src={Props.imgBanner} alt="" />
+      <img src={Props.imgBanner} alt="image bannière" />
+      <h2 className="h2-banner"style={{textAlign:Props.positionText}}>{Props.textBanner}</h2>
     </div>
   );
 }
