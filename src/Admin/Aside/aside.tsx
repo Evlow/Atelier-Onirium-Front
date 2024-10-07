1ea4528c6dacfa898a93df6fff0afac282818aee
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-10-07 23:09:19 +0200

    add aside

---
+++ src/Admin/Aside/aside.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+
+export default function Aside() {
+  const asideItems = [
+    {
+      title: "Dashbord",
+      path: "/dashbord",
+    },
+    {
+      title: "Créations",
+      path: "/creations",
+    },{
+        title : "Corbeille",
+        path: "/corbeille"
+    }
+  ];
+
+  return <>
+  <Box>
+
+    
+  </Box>
+  
+  
+  </>;
+}
