8aec459917148098824f237f9869fc206e87b291
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2024-10-10 20:08:59 +0200

    Update files

--- src/Components/Basket/BasketPage.tsx
+++ src/Components/Basket/BasketPage.tsx
@@ -2,7 +2,18 @@ import { useEffect, useState } from "react";
 import { Basket } from "../../Models/Basket";
 import agent from "../../App/Api/agent";
 import LoadingComponent from "../Laoding/laodingComponent";
-import { Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Delete } from "@mui/icons-material";
 export default function BasketPage() {
   const [loading, setLoading] = useState(true);
   const [basket, setBasket] = useState<Basket | null>(null);
@@ -14,13 +25,56 @@ export default function BasketPage() {
       .finally(() => setLoading(false));
   }, []);
 
-  if(loading) return <LoadingComponent message ="Votre panier est en cours de chargement, veuillez patienter"></LoadingComponent>
-  if(!basket) return <Typography variant="h3" color="white" fontFamily={"alice"} >Votre panier est vide</Typography>
-  
+  if (loading)
+    return (
+      <LoadingComponent message="Votre panier est en cours de chargement, veuillez patienter"></LoadingComponent>
+    );
+  if (!basket)
+    return (
+      <Typography variant="h3" color="white" fontFamily={"alice"}>
+        Votre panier est vide
+      </Typography>
+    );
+
   return (
     <>
-    <Typography variant="h3" color="white" fontFamily={"alice"} >Buyer Id = {}</Typography>
-
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="right"> Créations</TableCell>
+              <TableCell align="right"> Prix</TableCell>
+              <TableCell align="right"> Quantité</TableCell>
+              <TableCell align="right"> Total</TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {basket.items.map((item) => (
+              <TableRow
+                key={item.creationId}
+                sx={{ "&:last-child td, &:last-child th": { borde: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {item.name}
+                </TableCell>
+                <TableCell align="right">
+                  {item.price}€
+                </TableCell>
+                <TableCell align="right"> {item.quantity}</TableCell>
+                <TableCell align="right">
+                  {((item.price) * item.quantity)}€
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton color="error">
+                    <Delete />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
-  )
+  );
 }
